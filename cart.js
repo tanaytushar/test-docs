f178ece65f200b75a02b6c5d95efa43e249374cf
Tanay Tushar <tanay.t.tushar@gmail.com>
2026-03-18 23:32:27 +0530

    Add reason parameter to removeFromCart function

--- cart.js
+++ cart.js
@@ -2,8 +2,8 @@ function addToCart(productId, quantity, price, discount, note) {
   return { productId: productId, quantity: quantity, price: price, discount: discount, note: note };
 }
 
-function removeFromCart(productId) {
-  return { removed: productId };
+function removeFromCart(productId, reason) {
+  return { removed: productId, reason: reason };
 }
 
 function clearCart() {
